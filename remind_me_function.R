d7200736662ca0edc89ad638b71c355297024d9f
ChiaraRic <144387006+ChiaraRic@users.noreply.github.com>
2024-01-25 13:13:15 +0100

    Add files via upload

---
+++ remind_me_function.R
@@ -0,0 +1,14 @@
+# returns shopping list for vegan lasagna
+remind_me <- function(){
+  lasagna_recipe <-
+    c("lasagna plates",
+      "oat cream",
+      "tomato sauce",
+      "veggie minced meat",
+      "carrot", 
+      "red bell pepper",
+      "flour", 
+      "vegan cheese")
+  return(lasagna_recipe)
+}
+
